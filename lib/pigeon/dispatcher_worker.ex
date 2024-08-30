ab4653c3ea75505f4e12385c35594a4e0eb58ffb
Valerio Pizzichini <valerio.pizzichini@remote.com>
2024-08-30 14:29:35 +0200

    fix: avoid error and remove internal state from being logged

--- lib/pigeon/dispatcher_worker.ex
+++ lib/pigeon/dispatcher_worker.ex
@@ -1,5 +1,6 @@
 defmodule Pigeon.DispatcherWorker do
   @moduledoc false
+  require Logger
 
   use GenServer
 
@@ -40,7 +41,7 @@ defmodule Pigeon.DispatcherWorker do
         {:noreply, %{adapter: adapter, state: new_state}}
 
       {:stop, reason} ->
-        {:stop, {:shutdown, {reason, %{adapter: adapter, state: state}}}}
+        {:stop, {:shutdown, reason}, %{adapter: adapter, state: state}}
 
       {:stop, reason, new_state} ->
         {:stop, reason, %{adapter: adapter, state: new_state}}
